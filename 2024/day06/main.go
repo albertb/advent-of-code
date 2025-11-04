5390109b7627baa5a30aa2dcd06b1e274af1fff2
Albert Bachand <albertb@gmail.com>
2025-11-04 08:59:37 -0500

    2024 day 6

--- 2024/day06/main.go
+++ 2024/day06/main.go
@@ -11,35 +11,32 @@ var input string
 
 func main() {
 	fmt.Println("Part 1:", part1(input))
+	fmt.Println("Part 2:", part2(input))
 }
 
 type State struct {
-	walls  [][]bool
+	grid   [][]rune
 	x, y   int
 	dir    rune
-	visits [][]bool
+	visits map[string]bool
 }
 
 func parse(input string) State {
 	var result State
 	j := 0
-	for _, line := range strings.Split(input, "\n") {
+	for line := range strings.SplitSeq(input, "\n") {
 		if len(line) < 1 {
 			continue
 		}
 
-		visitRow := []bool{}
-
-		row := []bool{}
+		row := []rune{}
 		for i, rune := range line {
-			visitRow = append(visitRow, false)
-
 			if rune == '#' {
-				row = append(row, true)
+				row = append(row, '#')
 				continue
 			}
 
-			row = append(row, false)
+			row = append(row, '.')
 			if rune == '.' {
 				continue
 			}
@@ -48,17 +45,15 @@ func parse(input string) State {
 			result.y = j
 			result.dir = rune
 		}
-
-		result.visits = append(result.visits, visitRow)
-		result.walls = append(result.walls, row)
+		result.grid = append(result.grid, row)
 		j++
 	}
-	result.visits[result.y][result.x] = true
-
+	result.visits = make(map[string]bool)
 	return result
 }
 
-func (s *State) Next() bool {
+// Returns 1. whether the guard can still move, 2. whether the guard looped
+func (s *State) Next() (bool, bool) {
 	dx, dy := 0, 0
 	switch s.dir {
 	case '^':
@@ -72,12 +67,12 @@ func (s *State) Next() bool {
 	}
 
 	x, y := s.x+dx, s.y+dy
-	if x < 0 || y < 0 || y >= len(s.walls) || x >= len(s.walls[y]) {
+	if x < 0 || y < 0 || y >= len(s.grid) || x >= len(s.grid[y]) {
 		// Out of bounds.
-		return false
+		return false, false
 	}
 
-	if s.walls[y][x] {
+	if s.grid[y][x] == '#' {
 		// Obstactle in the way. Turn right!
 		switch s.dir {
 		case '^':
@@ -89,19 +84,40 @@ func (s *State) Next() bool {
 		case '<':
 			s.dir = '^'
 		}
-		return true
+		s.grid[s.y][s.x] = '+'
+		return true, false
+	}
+
+	visit := '+'
+	if dx == 0 {
+		visit = '|'
+	}
+	if dy == 0 {
+		visit = '-'
+	}
+
+	key := fmt.Sprintf("%d:%d,%d:%d", dx, x, dy, y)
+	if s.visits[key] {
+		return true, true
+	}
+	s.visits[key] = true
+
+	if s.grid[y][x] == '.' {
+		s.grid[y][x] = visit
+	}
+	if s.grid[y][x] != visit {
+		s.grid[y][x] = '+'
 	}
 
 	s.x, s.y = x, y
-	s.visits[y][x] = true
-	return true
+	return true, false
 }
 
 func (s State) Visited() int {
 	visits := 0
-	for _, row := range s.visits {
-		for _, visit := range row {
-			if visit {
+	for _, row := range s.grid {
+		for _, loc := range row {
+			if loc == '-' || loc == '|' || loc == '+' {
 				visits++
 			}
 		}
@@ -109,23 +125,15 @@ func (s State) Visited() int {
 	return visits
 }
 
-func (s State) ToString() string {
+func (s State) String() string {
 	var sb strings.Builder
-	sb.WriteString("State:")
-	for j, row := range s.walls {
+	sb.WriteString("State:\n")
+	for j, row := range s.grid {
 		for i, col := range row {
 			if s.x == i && s.y == j {
 				sb.WriteRune(s.dir)
 			} else {
-				if col {
-					sb.WriteRune('#')
-				} else {
-					if s.visits[j][i] {
-						sb.WriteRune('X')
-					} else {
-						sb.WriteRune('.')
-					}
-				}
+				sb.WriteRune(col)
 			}
 		}
 		sb.WriteString("\n")
@@ -135,13 +143,80 @@ func (s State) ToString() string {
 
 func part1(input string) int {
 	state := parse(input)
-	fmt.Println(state.ToString())
+	//fmt.Println(state.String())
 	for {
-		next := state.Next()
-		//fmt.Println(state.ToString())
+		next, _ := state.Next()
+		//fmt.Println(state.String())
 		if !next {
 			break
 		}
 	}
 	return state.Visited()
+}
+
+func (s State) Loops() bool {
+	//fmt.Println("MODIFIED", s.String())
+	for {
+		next, loop := s.Next()
+		//fmt.Println("LOOP", s.String())
+		if !next {
+			//fmt.Println("OOB")
+			return false
+		}
+		if loop {
+			//fmt.Println("LOOP")
+			return true
+		}
+	}
+}
+
+func (s State) Copy() State {
+	grid := make([][]rune, len(s.grid))
+	for i, row := range s.grid {
+		grid[i] = make([]rune, len(row))
+		copy(grid[i], row)
+	}
+	return State{
+		grid:   grid,
+		x:      s.x,
+		y:      s.y,
+		dir:    s.dir,
+		visits: make(map[string]bool),
+	}
+}
+
+func part2(input string) int {
+	start := parse(input)
+
+	state := start.Copy()
+	for {
+		next, _ := state.Next()
+		if !next {
+			break
+		}
+	}
+
+	tries := float64(len(state.grid)*len(state.grid[0]) - 1)
+	try := 0.0
+
+	count := 0
+	for j, row := range state.grid {
+		for i, loc := range row {
+			if start.x == i && start.y == j {
+				continue
+			}
+			if loc == '-' || loc == '|' || loc == '+' {
+				modified := start.Copy()
+				modified.grid[j][i] = '#'
+				if modified.Loops() {
+					modified.grid[j][i] = 'O'
+					count++
+				}
+			}
+			fmt.Printf("%f\n", try/tries)
+			try++
+		}
+	}
+
+	return count
 }
--- 2024/day06/main_test.go
+++ 2024/day06/main_test.go
@@ -47,3 +47,29 @@ func Test_part1(t *testing.T) {
 		})
 	}
 }
+
+func Test_part2(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{
+			name:  "simple",
+			input: simple,
+			want:  1,
+		},
+		{
+			name:  "example",
+			input: example,
+			want:  6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part2(tt.input); got != tt.want {
+				t.Errorf("part2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
