bd8c5056f560e2738a47835b93203b0178c0ef5a
Albert Bachand <albertb@gmail.com>
2025-11-07 12:56:29 -0500

    day 10 part 2

--- 2024/day10/day10.go
+++ 2024/day10/day10.go
@@ -13,7 +13,7 @@ var input string
 
 func main() {
 	fmt.Println("Part 1:", part1(input))
-	//fmt.Println("Part 2:", part2(input))
+	fmt.Println("Part 2:", part2(input))
 }
 
 func parse(input string) [][]int {
@@ -77,6 +77,58 @@ func part1(input string) int {
 				tops := map[Coord]struct{}{}
 				route(grid, x, y, 0, tops)
 				sum += len(tops)
+			}
+		}
+	}
+	return sum
+}
+
+type Path []Coord
+
+func (p Path) String() string {
+	sb := strings.Builder{}
+	for _, coord := range p {
+		sb.WriteString(fmt.Sprintf("(%d,%d)", coord.x, coord.y))
+	}
+	return sb.String()
+}
+
+func route2(grid [][]int, x, y, height int, path Path, trails map[string]struct{}) {
+	if x < 0 || y < 0 || y >= len(grid) || x >= len(grid[0]) {
+		// Out of bounds, give up.
+		return
+	}
+
+	if grid[y][x] != height {
+		// Unexpected height, we cannot reach the top from here.
+		return
+	}
+
+	// We found the next height, grow the path with this coordinate.
+	path = append(path, Coord{x, y})
+
+	if height == 9 {
+		// We reached the top, add this trail to the map.
+		trails[path.String()] = struct{}{}
+		return
+	}
+
+	// Route to each direction: left, up, right, down, looking for the next height.
+	for _, delta := range [][]int{{-1, 0}, {0, -1}, {1, 0}, {0, 1}} {
+		route2(grid, x+delta[0], y+delta[1], height+1, path, trails)
+	}
+}
+
+func part2(input string) int {
+	var sum int
+	grid := parse(input)
+
+	for y, row := range grid {
+		for x, height := range row {
+			if height == 0 {
+				trails := map[string]struct{}{}
+				route2(grid, x, y, 0, Path{}, trails)
+				sum += len(trails)
 			}
 		}
 	}
