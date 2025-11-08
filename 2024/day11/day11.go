36cdb34947703101ca3b3ab0bd3ceb04206508b8
Albert Bachand <albertb@gmail.com>
2025-11-08 18:05:39 -0500

    day 11 part 2 wip cache

--- 2024/day11/day11.go
+++ 2024/day11/day11.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"fmt"
 	"log"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -13,7 +14,8 @@ var puzzle string
 
 func main() {
 	fmt.Println("1:", part1(puzzle, 25))
-	fmt.Println("2:", part1(puzzle, 75))
+	fmt.Println("X:", part2(puzzle, 46))
+	//fmt.Println("2:", part2(puzzle, 75))
 }
 
 func parse(input string) []int64 {
@@ -28,43 +30,107 @@ func parse(input string) []int64 {
 	return numbers
 }
 
+var powerOf10 = []int64{
+	1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000,
+	10000000000, 100000000000, 1000000000000, 10000000000000, 100000000000000,
+	1000000000000000, 10000000000000000, 100000000000000000, 1000000000000000000,
+}
+
 func blink(before []int64) []int64 {
-	var after []int64
-	for _, number := range before {
-		if number == 0 {
-			after = append(after, 1)
+	after := make([]int64, len(before)*2)
+
+	index := 0
+	for _, stone := range before {
+		if stone == 0 {
+			after[index] = 1
+			index++
 			continue
 		}
 
-		mustParseInt := func(s string) int64 {
-			number, err := strconv.ParseInt(s, 10, 64)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			return number
-		}
+		digits := int(math.Log10(float64(stone))) + 1
 
-		s := strconv.FormatInt(number, 10)
-		len := len(s)
-		if len%2 == 0 {
-			lhs := mustParseInt(s[:len/2])
-			after = append(after, lhs)
-			rhs := mustParseInt(s[len/2:])
-			after = append(after, rhs)
+		if digits > 1 && digits%2 == 0 {
+			splitPower := powerOf10[digits/2]
+
+			lhs := stone / splitPower
+			after[index] = lhs
+
+			rhs := stone % splitPower
+			after[index+1] = rhs
+
+			index += 2
 			continue
 		}
 
-		after = append(after, number*2024)
+		after[index] = stone * 2024
+		index++
 	}
-	return after
+	return after[:index]
+}
+
+func singleBlink(stone int64) (int64, int64) {
+	if stone == 0 {
+		return 1, -1
+	}
+
+	digits := int(math.Log10(float64(stone))) + 1
+
+	if digits > 1 && digits%2 == 0 {
+		splitPower := powerOf10[digits/2]
+
+		lhs := stone / splitPower
+		rhs := stone % splitPower
+
+		return lhs, rhs
+	}
+
+	return stone * 2024, -1
+}
+
+type CacheEntry struct {
+	left, right int64
+}
+
+func countStones(stone int64, depth int, cache *map[int64]CacheEntry) int {
+	var left, right int64
+	if cached, ok := (*cache)[stone]; ok {
+		left, right = cached.left, cached.right
+	} else {
+		left, right = singleBlink(stone)
+	}
+
+	if depth == 1 {
+		if right >= 0 {
+			return 2
+		}
+		return 1
+	}
+
+	count := countStones(left, depth-1, cache)
+	if right >= 0 {
+		count += countStones(right, depth-1, cache)
+	}
+	return count
 }
 
 func part1(input string, blinks int) int {
-	numbers := parse(input)
+	stones := parse(input)
 
 	for i := range blinks {
-		fmt.Println("blink", i+1, "of", blinks)
-		numbers = blink(numbers)
+		fmt.Println(i + 1)
+		stones = blink(stones)
 	}
-	return len(numbers)
+	return len(stones)
+}
+
+func part2(input string, blinks int) int {
+	stones := parse(input)
+
+	sum := 0
+	cache := make(map[int64]CacheEntry)
+	for i, stone := range stones {
+		fmt.Println(i)
+		sum += countStones(stone, blinks, &cache)
+	}
+	return sum
 }
