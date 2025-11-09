3d9e7c8fed6af7ae674db476528dd97037188e13
Albert Bachand <albertb@gmail.com>
2025-11-09 08:48:29 -0500

    day 12 part 1

---
+++ 2024/day12/day12_test.go
@@ -0,0 +1,53 @@
+package main
+
+import "testing"
+
+func Test_part1(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{
+			name: "first",
+			input: `
+AAAA
+BBCD
+BBCC
+EEEC`,
+			want: 140,
+		},
+		{
+			name: "second",
+			input: `
+OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO`,
+			want: 772,
+		},
+		{
+			name: "larger",
+			input: `
+RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE`,
+			want: 1930,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part1(tt.input); got != tt.want {
+				t.Errorf("part1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
