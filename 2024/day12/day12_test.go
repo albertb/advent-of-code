28eece6a98796b5e108101eeb08075df688f49af
Albert Bachand <albertb@gmail.com>
2025-11-10 09:58:36 -0500

    day 12 part 2

--- 2024/day12/day12_test.go
+++ 2024/day12/day12_test.go
@@ -51,3 +51,59 @@ MMMISSJEEE`,
 		})
 	}
 }
+
+func Test_part2(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{
+			name: "first",
+			input: `
+AAAA
+BBCD
+BBCC
+EEEC`,
+			want: 80,
+		},
+		{
+			name: "second",
+			input: `
+OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO`,
+			want: 436,
+		},
+		{
+			name: "e-shaped",
+			input: `
+EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE`,
+			want: 236,
+		},
+		{
+			name: "two b",
+			input: `
+AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA`,
+			want: 368,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part2(tt.input); got != tt.want {
+				t.Errorf("part2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
