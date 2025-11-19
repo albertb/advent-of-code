5d1bf19d069f00cbd7f7e5b26c6253aaac14522b
Albert Bachand <albertb@gmail.com>
2025-11-19 16:02:50 -0500

    day 20 faster

--- 2024/day20/day20.go
+++ 2024/day20/day20.go
@@ -4,7 +4,6 @@ import (
 	"container/heap"
 	_ "embed"
 	"fmt"
-	"math"
 	"strings"
 
 	"github.com/albertb/advent-of-code/mathy"
@@ -45,199 +44,123 @@ func parse(input string) Maze {
 	return m
 }
 
-func heuristic(from mathy.Vec, to mathy.Vec) int {
-	return mathy.Abs(to.X-from.X) + mathy.Abs(to.Y-from.Y)
-}
+func dijkstra(m Maze, from mathy.Vec) map[mathy.Vec]int {
+	costs := map[mathy.Vec]int{}
+	costs[from] = 0
 
-type AStarNode struct {
-	pos    mathy.Vec
-	cost   int
-	rank   int
-	parent *AStarNode
-}
+	item := mathy.PriorityItem[mathy.Vec]{
+		Value:    from,
+		Priority: 0,
+	}
 
-type AstarHeap []*AStarNode
-
-func (h AstarHeap) Len() int           { return len(h) }
-func (h AstarHeap) Less(i, j int) bool { return h[i].rank < h[j].rank }
-func (h AstarHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *AstarHeap) Push(item any) {
-	*h = append(*h, item.(*AStarNode))
-}
-
-func (h *AstarHeap) Pop() any {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
-
-func astar(m Maze) int {
-	nm := map[mathy.Vec]*AStarNode{}
-	pq := &AstarHeap{}
+	pq := &mathy.PriorityQueue[mathy.Vec]{}
 	heap.Init(pq)
 
-	node := AStarNode{
-		pos:  m.start,
-		cost: 0,
-		rank: heuristic(m.start, m.end),
-	}
-	heap.Push(pq, &node)
+	heap.Push(pq, &item)
 
 	for pq.Len() > 0 {
-		current := heap.Pop(pq).(*AStarNode)
+		current := heap.Pop(pq).(*mathy.PriorityItem[mathy.Vec])
 
-		if current.pos.Equals(m.end) {
-			return current.cost
-		}
+		where := current.Value
+		nextCost := 1 + current.Priority
 
-		for _, dir := range mathy.RightLeftUpDown() {
-			next := current.pos.Plus(dir)
+		for _, dir := range mathy.Cardinals() {
+			next := where.Plus(dir)
 
 			if next.X < 0 || next.Y < 0 || next.X > m.bounds.X || next.Y > m.bounds.Y {
 				// Out of bounds.
 				continue
 			}
 
-			cost := 1 + current.cost
-
 			if _, ok := m.walls[next]; ok {
+				// Cannot go through walls.
 				continue
 			}
 
-			if node, ok := nm[next]; !ok {
-				node := AStarNode{
-					pos:    next,
-					cost:   cost,
-					rank:   cost + heuristic(next, m.end),
-					parent: current,
+			if cost, ok := costs[next]; !ok || nextCost < cost {
+				costs[next] = nextCost
+
+				item := mathy.PriorityItem[mathy.Vec]{
+					Value:    next,
+					Priority: nextCost,
 				}
-				nm[next] = &node
-				heap.Push(pq, &node)
-			} else if cost <= node.cost {
-				node.cost = cost
-				node.rank = cost + heuristic(next, m.end)
-				node.parent = current
-				heap.Push(pq, node)
+				heap.Push(pq, &item)
 			}
 		}
 	}
-
-	return math.MaxInt
-}
-
-func dfs(m Maze, where, cheat mathy.Vec, cost, max int, visited map[mathy.Vec]struct{}) map[mathy.Vec]int {
-
-	// XXX
-	/*grid := make([][]rune, m.bounds.Y+1)
-	for y := range m.bounds.Y + 1 {
-		grid[y] = make([]rune, m.bounds.X+1)
-	}
-	grid[m.start.Y][m.start.X] = 'S'
-	grid[m.end.Y][m.end.X] = 'E'
-	for w := range m.walls {
-		grid[w.Y][w.X] = '#'
-	}
-	for v := range visited {
-		grid[v.Y][v.X] = 'O'
-	}
-	if !cheat.Equals(mathy.Vec{}) {
-		grid[cheat.Y][cheat.X] = '$'
-	}
-	grid[where.Y][where.X] = 'X'
-
-	fmt.Println("cost", cost)
-	for _, row := range grid {
-		for _, col := range row {
-			if col == 0 {
-				fmt.Print(".")
-			} else {
-				fmt.Print(string(col))
-			}
-		}
-		fmt.Println()
-	}
-	fmt.Scanln()*/
-	// XXX
-
-	if cost > max {
-		return map[mathy.Vec]int{}
-	}
-
-	if where.Equals(m.end) {
-		//fmt.Println("cheat", cheat, "cost", cost)
-		return map[mathy.Vec]int{cheat: cost}
-	}
-	visited[where] = struct{}{}
-
-	costs := map[mathy.Vec]int{}
-	for _, dir := range mathy.RightLeftUpDown() {
-		nextWhere := where.Plus(dir)
-		nextCost := cost + 1
-		nextCheat := cheat
-
-		if _, ok := visited[nextWhere]; ok {
-			// We already visited this node, give up on this branch.
-			continue
-		}
-
-		if nextWhere.X < 0 || nextWhere.Y < 0 || nextWhere.X > m.bounds.X || nextWhere.Y > m.bounds.Y {
-			// Out of bounds.
-			continue
-		}
-
-		if _, ok := m.walls[nextWhere]; ok {
-			if !nextCheat.Equals(mathy.Vec{}) {
-				// We used our cheat already, give up on this branch.
-				continue
-			}
-			// Try to use the cheat to get across this wall.
-			nextCheat = nextWhere
-			//nextCost++
-			next := nextWhere.Plus(dir)
-			if _, ok := m.walls[next]; ok {
-				// We hit a second wall, give up.
-				continue
-			}
-		}
-
-		nextCosts := dfs(m, nextWhere, nextCheat, nextCost, max, visited)
-		for k, v := range nextCosts {
-			if oldV, ok := costs[k]; !ok || oldV > v {
-				costs[k] = v
-			}
-		}
-	}
-	delete(visited, where)
 
 	return costs
 }
 
-func part1(input string, minSaving int) int {
+func part1(input string, min int) int {
 	m := parse(input)
 
-	cheapest := astar(m)
-	cheatCosts := dfs(m, m.start, mathy.Vec{}, 0, cheapest-minSaving, map[mathy.Vec]struct{}{})
+	fromStart := dijkstra(m, m.start)
+	fromEnd := dijkstra(m, m.end)
 
-	//XXX
-	/*costs := map[int]int{}
-	for _, path := range cheatPaths {
-		costs[path]++
-	}
-	for k, v := range costs {
-		fmt.Println("there are", v, "cheat(s) that saves", (shortestPath - k))
-	}*/
-	//XXX
+	cost := fromStart[m.end]
 
-	sum := 0
-	for _, cost := range cheatCosts {
-		if cost <= cheapest-minSaving {
-			sum++
+	cheats := 0
+	for where := range fromStart {
+		costStart := fromStart[where]
+
+		for _, dir := range mathy.Cardinals() {
+			// Jump two cells away.
+			next := where.Plus(dir).Plus(dir)
+
+			// If there's no end cost on that cell, either it's a wall or cannot reach the end.
+			nextCostEnd, ok := fromEnd[next]
+			if !ok {
+				continue
+			}
+
+			cheatCost := costStart + 2 + nextCostEnd
+			if cheatCost <= cost-min {
+				cheats++
+			}
 		}
 	}
-	return sum
+
+	return cheats
+}
+
+func shortcuts(from mathy.Vec, max int) map[mathy.Vec]int {
+	results := map[mathy.Vec]int{}
+	for x := -max; x <= max; x++ {
+		maxy := max - mathy.Abs(x)
+		for y := -maxy; y <= maxy; y++ {
+			results[mathy.Vec{X: from.X + x, Y: from.Y + y}] = mathy.Abs(x) + mathy.Abs(y)
+		}
+	}
+	return results
+}
+
+func part2(input string, min int) int {
+	m := parse(input)
+
+	fromStart := dijkstra(m, m.start)
+	fromEnd := dijkstra(m, m.end)
+	normalCost := fromStart[m.end]
+
+	cheats := 0
+	for where := range fromStart {
+		costStart := fromStart[where]
+
+		for next, len := range shortcuts(where, 20) {
+			// If there's no end cost on that cell, either it's a wall or cannot reach the end.
+			nextCostEnd, ok := fromEnd[next]
+			if !ok {
+				continue
+			}
+
+			cheatCost := costStart + len + nextCostEnd
+			if cheatCost <= normalCost-min {
+				cheats++
+			}
+		}
+	}
+
+	return cheats
 }
 
 //go:embed puzzle.txt
@@ -245,4 +168,5 @@ var puzzle string
 
 func main() {
 	fmt.Println("1:", part1(puzzle, 100))
+	fmt.Println("2:", part2(puzzle, 100))
 }
