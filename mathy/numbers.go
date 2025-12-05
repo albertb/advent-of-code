5b702b9c0cc020e9c1ce7b44ecc69839b690b2b5
Albert Bachand <albertb@gmail.com>
2025-12-05 10:31:31 -0500

    2025 day 05

--- mathy/numbers.go
+++ mathy/numbers.go
@@ -1,6 +1,10 @@
 package mathy
 
-import "math"
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
 
 func Abs(n int) int {
 	if n < 0 {
@@ -15,4 +19,26 @@ func Pow64(x, y int64) int64 {
 
 func Pow(x, y int) int {
 	return int(math.Pow(float64(x), float64(y)))
+}
+
+func MustParseInt(s string) int {
+	num, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		panic(fmt.Sprint("failed to parse", s, ":", err))
+	}
+	return int(num)
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
